ac17e2ebed8bde771a4efba7ac160ba5e11927a7
Chance Ball <chanceball143@gmail.com>
2024-01-29 21:42:24 -0600

    wip wiki scraper

---
+++ src/main/java/com/bestinslot/osrswiki/EquipmentSection.java
@@ -0,0 +1,7 @@
+package com.bestinslot.osrswiki;
+
+public class EquipmentSection
+{
+    private String imageUrl;
+    private String name;
+}
---
+++ src/main/java/com/bestinslot/osrswiki/EquipmentTableSection.java
@@ -0,0 +1,17 @@
+package com.bestinslot.osrswiki;
+
+import java.util.Map;
+
+public class EquipmentTableSection
+{
+    private String Title;
+
+    private Map<String, EquipmentSection[]> Table;
+
+    public EquipmentTableSection() {}
+
+    public EquipmentTableSection(String title, Map<String, EquipmentSection[]> table) {
+        this.Title = title;
+        this.Table = table;
+    }
+}
